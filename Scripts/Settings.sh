4d8cd9d8b29b5873e0b89006cc93bbeb9c9d69e0
xjqxziii <xjqxziii@163.com>
2024-09-11 16:05:35 +0800

    speedtest

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -14,9 +14,10 @@ CFG_FILE="./package/base-files/files/bin/config_generate"
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-# netdata版本
-#sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=1.33.1/g" ./feeds/packages/admin/netdata/Makefile
 
 
 #配置文件修改
